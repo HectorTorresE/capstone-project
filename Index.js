fb3e96f380fe397c75bb1a576ceaead4db609d61
Hectorjte <hectorjte@gmail.com>
2022-02-01 15:59:36 -0600

    Add desktop version and mobile menu

---
+++ Index.js
@@ -0,0 +1,20 @@
+const Menu = document.getElementById('MenuP');
+const HeaderM = document.getElementById('HeaderM');
+const btnMenu = document.getElementById('menu');
+const btnClose = document.getElementById('close');
+
+
+const openAndClose = function () {
+  if (Menu.classList.contains('mobileMenuInvis')) {
+    Menu.classList.remove('mobileMenuInvis');
+    Menu.classList.add('mobileMenuVis');
+    HeaderM.classList.add('mobileMenuInvis');
+  } else if (Menu.classList.contains('mobileMenuVis')) {
+    Menu.classList.add('mobileMenuInvis');
+    Menu.classList.remove('mobileMenuVis');
+    HeaderM.classList.remove('mobileMenuInvis');
+  }
+};
+
+btnMenu.addEventListener('click', openAndClose);
+btnClose.addEventListener('click', openAndClose);
